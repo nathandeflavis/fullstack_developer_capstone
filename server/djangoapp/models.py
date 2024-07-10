d8380b691e4dc099559f4f66496f0fe1887fac98
Nathan De Flavis <92447278+nathandeflavis@users.noreply.github.com>
2024-07-10 18:31:27 +0100

    Update models.py
    Implemented linter feedback.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,9 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-#from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
@@ -14,9 +15,10 @@ class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
-
+    
     def __str__(self):
         return self.name  # Return the name as the string representation
+
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -46,6 +48,6 @@ class CarModel(models.Model):
         ]
     )
     # Other fields as needed
-
+    
     def __str__(self):
         return self.name  # Return the name as the string representation
